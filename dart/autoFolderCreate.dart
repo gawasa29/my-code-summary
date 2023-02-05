c0901a0b73cf6fd8aaba16f490eb4b54d2c58d86
Me is gawasa <61944230+gawasa29@users.noreply.github.com>
2023-02-05 17:13:27 +0900

    fix : autoFoldr変更

--- dart/autoFolderCreate.dart
+++ dart/autoFolderCreate.dart
@@ -2,16 +2,26 @@ import 'dart:io';
 
 //libに入ってからターミナルでdart autoFolderCreate.dartで作成できる
 Future<void> main() async {
-  final features = 'features/home';
+  final features = 'Features';
+  final model = 'Model';
+  final common = 'Common';
+  final template = 'Template';
+  final theme = 'Theme';
 
-  await Directory('common').create(recursive: true);
-  await Directory('models').create(recursive: true);
+  await Directory(common).create(recursive: true);
+  await Directory(features).create(recursive: true);
+  await Directory(model).create(recursive: true);
+  await Directory(template).create(recursive: true);
+  await Directory(theme).create(recursive: true);
 
-  await File('${features}/controller/home_controller.dart')
+  await File('$common/Utils/utils.dart').create(recursive: true);
+  await File('$model/user_model.dart').create(recursive: true);
+  await File('$template/Widgets/loader.dart').create(recursive: true);
+  await File('$theme/pllete.dart').create(recursive: true);
+
+  await File('$features/Loading/Controller/loading_controller.dart')
       .create(recursive: true);
-  await File('${features}/repository/home_repository.dart')
+  await Directory('$features/Loading/Repository/').create(recursive: true);
+  await File('$features/Loading/Screen/loading_screen.dart')
       .create(recursive: true);
-  await File('${features}/screens/home_screen.dart').create(recursive: true);
-
-  await File('theme/pllete.dart').create(recursive: true);
 }
