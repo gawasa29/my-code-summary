a5f3d6460f5e94fb46678f41f783851f334d9ddd
Me is gawasa <yusei112929@gmail.com>
2023-09-12 14:15:48 +0900

    add shell

---
+++ shell/install_apps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# アプリのリスト
+apps=(
+    google-chrome
+    iterm2
+    cmd-eikana
+    deepl
+    notion
+    todoist
+    discord
+    visual-studio-code
+    kindle
+    displaylink
+)
+
+# 各アプリをインストール
+for app in "${apps[@]}"; do
+    brew install --cask "$app"
+done
+
+# 実行方法:
+# 1. このスクリプトを install_apps.sh という名前で保存します。
+# 2. ターミナルを開き、スクリプトが保存されたディレクトリに移動します。
+# 3. 実行権限を付与します: chmod +x install_apps.sh
+# 4. スクリプトを実行します: ./install_apps.sh
